bba9d2fb368af7ddd3056b0902d970d6351613f6
Daniel S. Victoriano <136003903+Symphogear102@users.noreply.github.com>
2023-06-23 11:17:08 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('nav a').on('click', function(e) {
+		if (this.hash !== '') {
+			e.preventDefault();
+			const hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function() {
+				window.location.hash = hash;
+			});
+		}
+	});
+});
+
+
